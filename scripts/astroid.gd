e21de0d9bf2d9a5932d3e62f6d24924034a3a15a
Markus Rye Foss <8709797+markusrf@users.noreply.github.com>
2024-11-16 21:56:34 +0100

    add asteroid explosion sound effect

--- scripts/astroid.gd
+++ scripts/astroid.gd
@@ -4,6 +4,9 @@ extends CharacterBody3D
 @onready var player: CharacterBody3D = $"../Player"
 @onready var astroidMesh: MeshInstance3D = $MeshInstance3D
 @onready var explosion: Node3D = $Explosion
+@onready var explosionAudio: AudioStreamPlayer = $AsteroidExplosion
+
+var is_dead: bool = false
 
 func _ready() -> void:
 	var playerPosition = player.position
@@ -13,9 +16,13 @@ func _ready() -> void:
 func _physics_process(delta: float) -> void:
 	for index in range(get_slide_collision_count()):
 		var collision = get_slide_collision(index)
-		if collision.get_collider():
+		if collision.get_collider() and !is_dead:
+			is_dead = true
+			if !explosionAudio.playing:
+				explosionAudio.play()
 			explosion.explode()
 			astroidMesh.hide()
 			await get_tree().create_timer(2.0).timeout
 			queue_free()
+			break
 	move_and_slide()
