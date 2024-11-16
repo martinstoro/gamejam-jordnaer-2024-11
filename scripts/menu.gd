287998510d5f2f65b8b098d33b6c9f7c7b1b973f
Mads <madsdr28+1337@gmail.com>
2024-11-16 17:45:17 +0100

    added main menu

---
+++ scripts/menu.gd
@@ -0,0 +1,11 @@
+extends Control
+
+
+func _on_start_pressed() -> void:
+	get_tree().change_scene_to_file("res://Main3D.tscn")
+
+func _on_quit_pressed() -> void:
+	get_tree().quit()
+
+func _on_credits_pressed() -> void:
+	pass
