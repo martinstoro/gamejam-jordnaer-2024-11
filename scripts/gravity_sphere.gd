420ec61bf6e76c95737e32dc648a259957ea39e0
KyrillieBillie <kyrill.falk+personal@gmail.com>
2024-11-16 19:49:22 +0100

    Tweaking
    Teweaking of gravity formula

--- scripts/gravity_sphere.gd
+++ scripts/gravity_sphere.gd
@@ -1,12 +1,21 @@
 extends StaticBody3D
 @onready var player: CharacterBody3D = $"../Player"
 
+enum {METEOR, PLANET, BLACK_HOLE}
+
 @export var IS_DANGER = true
-const GRAVITY_FORCE = 0.5
+@export var OBJECT_TYPE = PLANET
+@export var GRAVITY_FORCE = 10
+
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta: float) -> void:
 	var playerPosition = player.position
 	var direction = playerPosition - position
-	player.velocity -= direction.normalized() * scale*scale / direction.length() * GRAVITY_FORCE
+	var distance = direction.length()
+	var mass = scale * scale 
+	
+	var gravity = GRAVITY_FORCE * direction.normalized() * mass / (distance * distance)
+	
+	player.velocity -= gravity
 	
--- scripts/menu.gd
+++ scripts/menu.gd
@@ -4,7 +4,7 @@ func _ready() -> void:
 	start_menu()
 
 func _on_start_pressed() -> void:
-	get_tree().change_scene_to_file("res://Main3D.tscn")
+	get_tree().change_scene_to_file("res://Levels/level-1.tscn")
 
 func _on_quit_pressed() -> void:
 	get_tree().quit()
@@ -14,7 +14,7 @@ func _on_credits_pressed() -> void:
 
 func _process(delta):
 	if Input.is_key_pressed(KEY_R): 
-		get_tree().change_scene_to_file("res://Main3D.tscn")
+		get_tree().change_scene_to_file("res://Levels/level-1.tscn")
 
 func _on_back_pressed() -> void:
 	start_menu()
