62fb8e09e81b66089f2c7308af96f30181a2e416
Mads <madsdr28+1337@gmail.com>
2024-11-16 18:13:20 +0100

    fix menu touching

--- scripts/menu.gd
+++ scripts/menu.gd
@@ -1,5 +1,7 @@
 extends Control
 
+func _ready() -> void:
+	start_menu()
 
 func _on_start_pressed() -> void:
 	get_tree().change_scene_to_file("res://Main3D.tscn")
@@ -13,3 +15,19 @@ func _on_credits_pressed() -> void:
 func _process(delta):
 	if Input.is_key_pressed(KEY_R): 
 		get_tree().change_scene_to_file("res://Main3D.tscn")
+	credits_show()
+
+func _on_back_pressed() -> void:
+	start_menu()
+
+func start_menu() -> void:
+	$MarginContainer/VBoxContainer/Start.show()
+	$MarginContainer/VBoxContainer/Credits.show()
+	$MarginContainer/VBoxContainer/Quit.show()
+	$MarginContainer/VBoxContainer/Back.hide()
+	
+func credits_show() -> void:
+	$MarginContainer/VBoxContainer/Start.hide()
+	$MarginContainer/VBoxContainer/Credits.hide()
+	$MarginContainer/VBoxContainer/Quit.hide()
+	$MarginContainer/VBoxContainer/Back.show()
