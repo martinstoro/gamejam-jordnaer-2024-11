e9f09c6d66511bb2ea7e1f9e38647bb10e100131
Mads <madsdr28+1337@gmail.com>
2024-11-16 21:20:50 +0100

    add astroid speed

--- scripts/astroid.gd
+++ scripts/astroid.gd
@@ -1,9 +1,15 @@
 extends CharacterBody3D
 
 @export var IS_DANGER = true
+@onready var player: CharacterBody3D = $"../Player"
 @onready var astroidMesh: MeshInstance3D = $MeshInstance3D
 @onready var astroidCollision: CollisionShape3D = $CollisionShape3D
 @onready var explosion: Node3D = $Explosion
+
+func _ready() -> void:
+	var playerPosition = player.position
+	velocity = (playerPosition - position).normalized() * randf() * 100
+	velocity = velocity.rotated(Vector3.UP, randf())
 
 func _physics_process(delta: float) -> void:
 	for index in range(get_slide_collision_count()):
