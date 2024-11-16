2c6d749ed7e444025a15c89b4171fcc3f84a9e4c
Mads <madsdr28+1337@gmail.com>
2024-11-16 19:04:36 +0100

    add credits

--- scripts/menu.gd
+++ scripts/menu.gd
@@ -10,7 +10,7 @@ func _on_quit_pressed() -> void:
 	get_tree().quit()
 
 func _on_credits_pressed() -> void:
-		credits_show()
+	credits_show()
 
 func _process(delta):
 	if Input.is_key_pressed(KEY_R): 
@@ -24,9 +24,17 @@ func start_menu() -> void:
 	$MarginContainer/VBoxContainer/Credits.show()
 	$MarginContainer/VBoxContainer/Quit.show()
 	$MarginContainer/VBoxContainer/Back.hide()
+	$CreditKyrill.hide()
+	$CreditMads.hide()
+	$CreditMartin.hide()
+	$CreditMarkus.hide()
 	
 func credits_show() -> void:
 	$MarginContainer/VBoxContainer/Start.hide()
 	$MarginContainer/VBoxContainer/Credits.hide()
 	$MarginContainer/VBoxContainer/Quit.hide()
 	$MarginContainer/VBoxContainer/Back.show()
+	$CreditKyrill.show()
+	$CreditMads.show()
+	$CreditMartin.show()
+	$CreditMarkus.show()
