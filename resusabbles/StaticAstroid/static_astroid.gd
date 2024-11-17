008847736d95af0b4fa41a94bcf351d0d8959ff4
Mads <madsdr28+1337@gmail.com>
2024-11-17 15:12:28 +0100

    rotation on static astroids

--- resusabbles/StaticAstroid/static_astroid.gd
+++ resusabbles/StaticAstroid/static_astroid.gd
@@ -1,2 +1,17 @@
 extends AnimatableBody3D
 @export var IS_DANGER = true
+
+var rotationSpeed = 0.01
+var rotationVelocityX
+var rotationVelocityY
+var rotationVelocityZ
+
+func _ready():
+	rotationVelocityX = randf() * rotationSpeed
+	rotationVelocityY = randf() * rotationSpeed
+	rotationVelocityZ = randf() * rotationSpeed
+	
+func _physics_process(delta: float) -> void:
+	rotation.x += rotationVelocityX
+	rotation.y += rotationVelocityY
+	rotation.z += rotationVelocityZ
