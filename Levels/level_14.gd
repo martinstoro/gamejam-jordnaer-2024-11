59be0fd124c97f7fe0eca56984ef75943b53889a
Mads <madsdr28+1337@gmail.com>
2024-11-17 16:09:17 +0100

    push level 14 script

---
+++ Levels/level_14.gd
@@ -0,0 +1,13 @@
+extends Node3D
+
+@export var HAS_ASTROIDS = true
+@export var ASTROID_SPAWN_TIME = 0.5
+
+func _ready():
+	Globals.final()
+	
+	if HAS_ASTROIDS:
+		Globals.start_astroids()
+		Globals.astroid_spawn_time(ASTROID_SPAWN_TIME)
+	else:
+		Globals.stop_astroids()
